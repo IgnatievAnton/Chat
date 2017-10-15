90f36934d4cb5538dd118e850afd4349ad454d08
ignatievanton <ignatievanton1996@gmail.com>
2017-10-15 19:35:14 +0300

    New build Server version 2

--- Server/buildScript.sh
+++ Server/buildScript.sh
@@ -5,4 +5,9 @@ make
 i=$(( $(cat version.txt) + 1))
 echo $i > version.txt 
 echo program create
+cd ..
+git add --all
+git commit -m "New build Server version $i"
+git tag ServerVers$i
+git push -u origin master
 
