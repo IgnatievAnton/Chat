e1af894217b3ca274046d93c99b1c2b2b7d27385
ignatievanton <ignatievanton1996@gmail.com>
2017-10-15 19:30:46 +0300

    test

--- Server/buildScript.sh
+++ Server/buildScript.sh
@@ -2,5 +2,7 @@
 cmake ~/Desktop/c++/serverNix/
 echo program build
 make
+i=$(( $(cat version.txt) + 1))
+echo $i > version.txt 
 echo program create
 
--- Server/main.cpp
+++ Server/main.cpp
@@ -4,17 +4,19 @@
 #include <iostream>
 #include <unistd.h>
 
+#define port 8080
+
+
 
 using namespace std;
 int main()
 {
+    cout << "servers started"<<endl;
     int sock, listener;
     int nclient = 0;
     struct sockaddr_in addr;
     char buf[1024];
     int bytes_read;
-
-
     listener = socket(AF_INET, SOCK_STREAM, 0);
     if(listener < 0)
     {
@@ -22,39 +24,33 @@ int main()
         return 0;
     }
 
+    addr.sin_addr.s_addr = INADDR_ANY;
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(3425);
-    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port = htons(port);
     if(bind(listener, (struct sockaddr *)&addr, sizeof(addr)) < 0)
     {
         cout << "Socket bind ERROR!!!" << endl;
         return 0;
     }
-
     if(listen(listener, 20))
     {
         cout << "Socket listener ERROR!!!"<<endl;
         return 0;
     }
-
     while(1)
     {
         sock = accept(listener, NULL, NULL);
         nclient++;
         if(sock < 0)
-        {
             return 0;
-        }
         while(1)
         {
             bytes_read = recv(sock, buf, 1024, 0);
-            if(bytes_read <= 0) break;
+            if(bytes_read <= 0)
+                break;
             send(sock, buf, bytes_read, 0);
         }
-
-
         close(sock);
     }
-
     return 0;
 }
