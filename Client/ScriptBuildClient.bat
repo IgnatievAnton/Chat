7b8a51d4b512a2995dd81df42095d265c5f20a95
IgnatievAnton <ignatievanton1996@gmail.com>
2017-10-16 21:34:02 +0300

    New build clientProject version 2

--- Client/ScriptBuildClient.bat
+++ Client/ScriptBuildClient.bat
@@ -6,13 +6,5 @@ copy nul ServerVersion.txt
 ECHO %vers% >>ServerVersion.txt
 qmake -o Makefile chatterbox.pro
 make
-d:
-cd qt\5.6\msvc2015_64\bin
-windeployqt.exe C:\git\Chat\Client\relese\chatterbox.exe
 echo exe file create on path /relese.
-c: 
-cd C:\git\Chat\Client
 git.bat
-pause
-pause
-pause
